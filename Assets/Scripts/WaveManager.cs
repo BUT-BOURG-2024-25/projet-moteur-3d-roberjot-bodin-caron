379dab9fc351bd470fc49cf650cca5a694541b67
p2203330 <alois.roberjot@etu.univ-lyon1.fr>
2024-12-10 10:21:27 +0100

    wave manager

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WaveManager : MonoBehaviour
+public class WaveManager : Singleton<WaveManager>
 {
     [SerializeField]
     private Text WaveText;
